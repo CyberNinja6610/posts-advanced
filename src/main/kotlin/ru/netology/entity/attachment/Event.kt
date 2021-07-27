2685b83f264c8dbfc6c6f5f03ee78837d56c5d85
SpaceCadet <spacecadet2702@gmail.com>
2021-07-27 21:23:44 +0500

    rm equels methods

--- src/main/kotlin/ru/netology/entity/attachment/Event.kt
+++ src/main/kotlin/ru/netology/entity/attachment/Event.kt
@@ -11,32 +11,4 @@ data class Event(
     val button_text: String,
     val friends: Array<String>,
 
-    ) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other !is Event) return false
-
-        if (id != other.id) return false
-        if (time != other.time) return false
-        if (memberStatus != other.memberStatus) return false
-        if (isFavorite != other.isFavorite) return false
-        if (address != other.address) return false
-        if (text != other.text) return false
-        if (button_text != other.button_text) return false
-        if (!friends.contentEquals(other.friends)) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = id.hashCode()
-        result = (31 * result + time).toInt()
-        result = 31 * result + memberStatus
-        result = 31 * result + isFavorite.hashCode()
-        result = 31 * result + address.hashCode()
-        result = 31 * result + text.hashCode()
-        result = 31 * result + button_text.hashCode()
-        result = 31 * result + friends.contentHashCode()
-        return result
-    }
-}
+    )
